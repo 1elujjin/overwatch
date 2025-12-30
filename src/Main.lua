4867a2d85827029f5b1d3c6c6192123a22855f9d
1elujjin <1@elujj.in>
2025-12-30 01:35:27 +0000

    Fixed execution to only reference internal files

--- src/Main.lua
+++ src/Main.lua
@@ -1,7 +1,7 @@
 --[[
 
-	AirHub V2 by Exunys © CC0 1.0 Universal (2023)
-	https://github.com/Exunys
+	Overwatch by Norgates © CC0 1.0 Universal (2026)
+	https://discord.gg/norgates
 
 ]]
 
@@ -25,11 +25,11 @@ local osdate = os.date
 
 --// Launching
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Roblox-Functions-Library/main/Library.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/1elujjin/overwatch/main/Library.lua"))()
 
-local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/AirHub-V2/main/src/UI%20Library.lua"))()
-local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Exunys-ESP/main/src/ESP.lua"))()
-local Aimbot = loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Aimbot-V3/main/src/Aimbot.lua"))()
+local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/1elujjin/overwatch/main/src/UI%20Library.lua"))()
+local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/1elujjin/overwatch/main/src/ESP.lua"))()
+local Aimbot = loadstring(game:HttpGet("https://raw.githubusercontent.com/1elujjin/overwatch/main/src/Aimbot.lua"))()
 
 --// Variables
 
