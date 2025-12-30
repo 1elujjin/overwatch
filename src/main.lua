e7f0791767a38122f348bc80869c51e8a16ea52d
1elujjin <1@elujj.in>
2025-12-30 01:19:10 +0000

    Fixed references

--- src/main.lua
+++ src/main.lua
@@ -25,11 +25,11 @@ local osdate = os.date
 
 --// Launching
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Roblox-Functions-Library/main/Library.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/1elujjin/exeter/refs/heads/main/src/library.lua"))()
 
-local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/AirHub-V2/main/src/UI%20Library.lua"))()
+local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/1elujjin/exeter/refs/heads/main/src/gui.lua"))()
 --// local ESP = loadstring(game:HttpGet("ESP.lua"))()
-local Aimbot = loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Aimbot-V3/main/src/Aimbot.lua"))()
+local Aimbot = loadstring(game:HttpGet("https://raw.githubusercontent.com/1elujjin/exeter/refs/heads/main/src/aimbot.lua"))()
 
 --// Variables
 
